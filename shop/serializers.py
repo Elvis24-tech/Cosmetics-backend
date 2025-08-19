9e3fa467ba6d93506ed8d021a840906a97dd5ed1
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-08-20 00:25:51 +0300

    cloudinary added

--- shop/serializers.py
+++ shop/serializers.py
@@ -2,9 +2,31 @@ from rest_framework import serializers
 from .models import Product, Order
 
 class ProductSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
+
+        fields = [
+            'id', 
+            'name', 
+            'description', 
+            'price', 
+            'stock', 
+            'image',     
+            'image_url',  
+            'created_at'
+        ]
+
+    def get_image_url(self, obj):
+        """
+        Returns the full URL of the image from Cloudinary.
+        """
+       
+        if obj.image and hasattr(obj.image, 'url'):
+            return obj.image.url
+       
+        return None
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
